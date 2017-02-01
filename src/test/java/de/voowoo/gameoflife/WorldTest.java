a0842ffadb177afb6b41cb9f321db56687d5fbd1
Volker <voowoo0@gmail.com>
2017-02-01 22:14:48 +0100

    fixed 5x5 test

--- src/test/java/de/voowoo/gameoflife/WorldTest.java
+++ src/test/java/de/voowoo/gameoflife/WorldTest.java
@@ -157,9 +157,9 @@ public class WorldTest
         world.cycle();
 
         assertThat(world, is(new World(new boolean[][]{{false, false, false, false, false},
-                                                       {false, false, true, false, false},
+                                                       {false, false, false, false, false},
                                                        {false, false, true, true, false},
-                                                       {false, false, true, false, false},
+                                                       {false, false, false, false, false},
                                                        {false, false, false, false, false}})));
     }
 
